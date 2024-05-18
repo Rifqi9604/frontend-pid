c556113214a2383e80cd43b0945e737a3360be68
Ramdhik <ramadhikadarma0607@gmail.com>
2024-05-18 16:32:38 +0700

    Menambahkan Warna Secondary(orange) dan purple

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,21 +2,26 @@ import { nextui } from '@nextui-org/react';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        purple: {},
+        purple: {
+          50: '#F0F1FD',
+          100: '#D2D3F8',
+          200: '#BCBEF5',
+          300: '#9DA1F1',
+          400: '#8A8EEE',
+          500: '#6D72EA',
+          600: '#6368D5',
+          700: '#4D51A6',
+          800: '#3C3F81',
+          900: '#2E3062',
+        },
         blue: {},
         pink: {},
         neutral: {},
@@ -32,7 +37,20 @@ const config: Config = {
           extend: 'light',
           colors: {
             primary: {},
-            secondary: {},
+            secondary: {
+              50: '#FFEFEA',
+              100: '#FFCFBE',
+              200: '#FFB79F',
+              300: '#FE9673',
+              400: '#FE8258',
+              500: '#FE632E',
+              600: '#E75A2A',
+              700: '#B44621',
+              800: '#8C3619',
+              900: '#6B2A13',
+              DEFAULT: '#FE632E',
+              foreground: '#ffffff',
+            },
             warning: {},
             danger: {},
             success: {},
